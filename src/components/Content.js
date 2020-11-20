5ae42857595efe3bc26d2e4a9c28132b337fca7d
Martin Kuchar <kucharmartin0@gmail.com>
2020-11-19 20:30:15 -0800

    Add about me

---
+++ src/components/Content.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Col from 'react-bootstrap/esm/Col';
+import Container from 'react-bootstrap/esm/Container';
+import Row from 'react-bootstrap/esm/Row';
+
+function Content(props) {
+    return(
+        <Container fluid={true}>
+            <Row className="justify-content-center">
+                <Col md={8}>
+                    {/* Everything we put inside 'Content' tags */}
+                    {props.children}
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Content;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Content from '../components/Content';
+import Myself from '../components/Myself';
 
-function About() {
+function About(props) {
     return(
-        <p>About Works!</p>
+        <div>
+            <Myself tittle={props.tittle} />
+            <Content>
+                <p>I am a dedicated software engineer with creative problem-solving skills.<br/>
+                Currently, I am seeking opportunities that will allow proceeding in the way to become a qualified software engineer.</p>
+                <p>My expertise also includes drawing in solid work design software and building projects in HVAC systems. </p>
+                
+                <p>I love coding and I hope that once I will create something that people will love to use!</p>
+            </Content>
+        </div>
     );
 };
 
