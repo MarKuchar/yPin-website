f519e252fa9c10cd27995e7d5b6f531b57e65cfa
Martin Kuchar <kucharmartin0@gmail.com>
2020-11-19 18:39:19 -0800

    Add footer to the website

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-
 import './App.css';
+
+// Import the components I've made
+import Footer  from './components/Footer';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -49,6 +51,10 @@ class App extends React.Component {
                 </Nav>
               </Navbar.Collapse>
             </Navbar>
+
+            <Footer/>
+
+
           </Container>
       </Router>
     );
