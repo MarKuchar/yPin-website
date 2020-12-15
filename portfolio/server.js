4a028f59968d7fbcc190ce196f222075893bf05b
MarKuchar <kucharmartin0@gmail.com>
2020-12-15 07:38:29 +0000

    das

--- portfolio/server.js
+++ portfolio/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.set('port', (process.env.PORT || 4i3))
+app.set('port', (process.env.PORT || 443))
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
