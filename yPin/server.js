8abcdac7542dfc4e6a3894254c599198a0e067c7
Martin Kuchar <kucharmartin0@gmail.com>
2020-12-21 20:13:28 -0800

    Change some

--- yPin/server.js
+++ yPin/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.set('port', (process.env.PORT || 443))
-app.use(express.static(path.join(__dirname, 'build')));
+app.set('port', (process.env.PORT || 80))
+app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
